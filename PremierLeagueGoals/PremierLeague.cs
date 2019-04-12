ff2d770fd02d075ceaaef1e65bdc2723f5018374
John Zissimou <jzissimou@travco.co.uk>
2019-04-12 14:43:48 +0100

    First commit

---
+++ PremierLeagueGoals/PremierLeague.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PremierLeagueGoals
+{
+    class PremierLeague
+    {
+        public string Name { get; set; }
+        public List<Rounds> Rounds { get; set; }
+    }
+
+    class Rounds
+    {
+        public string Name { get; set; }
+        public List<Matches> Matches { get; set; }
+    }
+
+    class Matches
+    {
+        public DateTime Date { get; set; }
+        public Team1 Team1 { get; set; }
+        public Team2 Team2 { get; set; }
+        public int Score1 { get; set; }
+        public int Score2 { get; set; }
+    }
+
+    class Team1
+    {
+        public string Key { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+    }
+
+    class Team2
+    {
+        public string Key { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+    }
+}
---
+++ PremierLeagueGoals/Program.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net;
+
+namespace PremierLeagueGoals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var totalGoals = 0;
+            string inputTeam = "manutd"; // Same as the key in JSON
+            var jsonInput = "";
+
+            using (WebClient wc = new WebClient())
+            {
+                jsonInput = wc.DownloadString("https://raw.githubusercontent.com/openfootball/football.json/master/2014-15/en.1.json");
+
+                var jsonData = JsonConvert.DeserializeObject<PremierLeague>(jsonInput);
+
+                foreach (var round in jsonData.Rounds)
+                {
+                    foreach (var match in round.Matches)
+                    {
+                        if (inputTeam == match.Team1.Key)
+                            totalGoals += match.Score1;
+                        else if (inputTeam == match.Team2.Key)
+                            totalGoals += match.Score2;
+                    }
+                }
+            }
+
+            Console.WriteLine(totalGoals.ToString());
+            Console.Read();
+        }
+    }
+}
