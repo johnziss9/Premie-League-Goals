8222001af75879990b42b176bdee43a90c2655d2
John Zissimou <jzissimou@travco.co.uk>
2019-04-15 09:15:37 +0100

    Added user input

--- PremierLeagueGoals/Program.cs
+++ PremierLeagueGoals/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 
 namespace PremierLeagueGoals
@@ -8,8 +10,8 @@ namespace PremierLeagueGoals
     {
         static void Main(string[] args)
         {
+            List<string> teamKeys = new List<string>();
             var totalGoals = 0;
-            string inputTeam = "manutd"; // Same as the key in JSON
             var jsonInput = "";
 
             using (WebClient wc = new WebClient())
@@ -18,16 +20,40 @@ namespace PremierLeagueGoals
 
                 var jsonData = JsonConvert.DeserializeObject<PremierLeague>(jsonInput);
 
-                foreach (var round in jsonData.Rounds)
+                foreach (var teamKey in jsonData.Rounds[0].Matches)
                 {
-                    foreach (var match in round.Matches)
+                    teamKeys.Add(teamKey.Team1.Key);
+                    teamKeys.Add(teamKey.Team2.Key);
+                }
+
+                var sortedTeamList = teamKeys.OrderBy(x => x).ToList();
+
+                Console.WriteLine("Please enter one of the following teams as listed below:");
+
+                foreach (var team in sortedTeamList)
+                {
+                    Console.WriteLine(team);
+                }
+
+                Console.Write(Environment.NewLine);
+
+                string teamInput = Console.ReadLine().ToString();
+
+                if (sortedTeamList.Contains(teamInput, StringComparer.OrdinalIgnoreCase))
+                {
+                    foreach (var round in jsonData.Rounds)
                     {
-                        if (inputTeam == match.Team1.Key)
-                            totalGoals += match.Score1;
-                        else if (inputTeam == match.Team2.Key)
-                            totalGoals += match.Score2;
+                        foreach (var match in round.Matches)
+                        {
+                            if (teamInput == match.Team1.Key)
+                                totalGoals += match.Score1;
+                            else if (teamInput == match.Team2.Key)
+                                totalGoals += match.Score2;
+                        }
                     }
                 }
+                else
+                    throw new Exception("Input didn't match any teams. Re-run the application to try again.");
             }
 
             Console.WriteLine(totalGoals.ToString());
